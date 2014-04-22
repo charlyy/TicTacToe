2e80a0e92e368371ddf0602b9a79bc92701a7f45
charlyy <cyoon90@yahoo.com>
2014-04-21 20:29:42 -0700

    first commit!!

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+function TicTacToeCtrl($scope){
+	 $scope.board = ['','','','','','','','',''];
+	//$scope.board = [0,0,0,0,0,0,0,0,0];
+
+  $scope.player = 1;
+// $scope.player1 = ;
+// $scope.player2 = ;
+// $scope.winners = [];
+
+$scope.togglePlayer = function(cellIndex){
+	if ($scope.player === 1){
+	//	$scope.player1.push(cellIndex);
+		$scope.board[cellIndex]="X";
+		//console.log(player1);
+
+		$scope.player = 2;
+
+
+	}
+	else {
+	//	$scope.player2.push(cellIndex);
+		$scope.board[cellIndex]="O";
+		//console.log(player2);
+
+		$scope.player = 1;
+
+	}
+
+}
+
+
+
+
+
+//function setCell(x, value) {
+    //    $scope.board[x] = value
+    //}
+
+
+
+} // End of controller
