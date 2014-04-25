dd97e9575af74a51bbb92d9ebfce40df162993b6
charlyy <cyoon90@yahoo.com>
2014-04-24 19:06:18 -0700

    need to fix player score and turns

--- js/script.js
+++ js/script.js
@@ -1,120 +1,150 @@
-var gameTTT = angular.module('gameTTT', []);
+var gameTTT = angular.module('gameTTT', ["firebase"]);
 
-gameTTT.controller('TicTacToeCtrl', function ($scope){
+gameTTT.controller('TicTacToeCtrl', function ($scope, $firebase){
 
 
 $scope.player = 1;
 var moves = 0;
 
-$scope.xWins = 0;
-$scope.oWins = 0;
-$scope.ties = 0;
+// $scope.xWins = 0;
+// $scope.oWins = 0;
+// $scope.ties = 0;
+// $scope.game.board = ['','','','','','','','',''];
+
+
+// Track if we're player 1 or 2
+var playerNum = null;
+
+    var ticTacRef = new Firebase("https://elfuego.firebaseio.com/");
+      var lastGame;
+      // Ask for all existing game info from firebase
+      ticTacRef.once('value', function(gamesSnapshot) {
+        // get the actual games data
+        var games = gamesSnapshot.val();
+        if(games == null)
+        {
+          // No games at all, so make a new game -- As if we're Areg
+          lastGame = ticTacRef.push( {waiting: true} );
+          playerNum = 1;
+        }
+        else  // I do have at least one game out there...
+        {
+          var keys = Object.keys(games);
+          var lastGameKey = keys[ keys.length - 1 ];
+          var lastGame = games[ lastGameKey ];
+          console.log("This person's game: " + lastGameKey);
+          if(lastGame.waiting)
+          {
+            // Currently someone is waiting -- Areg is there and we're Rocky
+            // Grab from Firebase its last game object
+            lastGame = ticTacRef.child(lastGameKey);
+            // Set a new game on this
+            lastGame.set( {xWins: 0, oWins: 0, ties: 0, waiting: false, moves: 0, won: false, board: ['','','','','','','','','']} );
+            playerNum = 2;
+          }
+          else
+          {
+            // Make a new game -- As if we're Areg
+            lastGame = ticTacRef.push( {waiting: true} );
+            playerNum = 1;
+          }
+        // Attach the last game to what we're up to
+        $scope.game = $firebase(lastGame);
+    };
+  });
 
 // Start at player one
   $scope.togglePlayer = function(cellIndex)
   {
-    if ($scope.board[cellIndex] == "")
+    if ($scope.game.board[cellIndex] == "")
       {
           if($scope.player == 1)
             {
-    
-              $scope.board[cellIndex] = 'X';
+              $scope.game.board[cellIndex] = 'X';
               console.log(cellIndex);
               moves++;
               winFunction();
               $scope.player = 2;
-              
             } 
             else 
               {
-
-				        $scope.board[cellIndex] = "O";
+				        $scope.game.board[cellIndex] = 'O';
                 console.log(cellIndex);
                 moves++;
                 winFunction();
                 $scope.player = 1;
               }
-
+              $scope.game.$save();
       }	
     else 
       {     
         alert("Can't move there!");
       }			
-    };
+  };
 
-// var winner = function(){
-//   if($scope.player ==1){
-//     $scope.board = ['X','X','X','X','X','X','X','X','X'];
-//     console.log('X wins!')
-//   } else {
-//     $scope.board = ['O','O','O','O','O','O','O','O','O'];
-//     console.log('O wins!')
-//   }
-// }
-
-  
     
 var tieGame = function(){
   $scope.ties++;
 }
 
-var youWon = function(){
-  if ($scope.player == 1){
+var youWon = function()
+{
+  if ($scope.player == 1)
+  {
     $scope.xWins++;
-    $scope.board = ['X','X','X','X','X','X','X','X','X'];
+    $scope.game.board = ['X','X','X','X','X','X','X','X','X'];
     console.log('X wins!');
-    document.getElementById('htmlBG').style.backgroundImage="url('../images/Xbg.png'), url('../images/xbg2.png');";
-  }else{
+
+  }
+  else
+  {
     $scope.oWins++;
-    $scope.board = ['O','O','O','O','O','O','O','O','O'];
+    $scope.game.board = ['O','O','O','O','O','O','O','O','O'];
     console.log('O wins!');
-    document.getElementById('htmlBG').style.backgroundImage="url('../images/obg.png'), url('../images/obg2.png');";
   };
 };
 
 
-$scope.clearBoard = function(){
-  $scope.board = ['','','','','','','','',''];
+$scope.clearBoard = function()
+{
+  $scope.game.board = ['','','','','','','','',''];
   moves = 0;
   $scope.player = 1;
 }
-
-
-
 
  var winFunction = function()
         {
           if (moves > 4){
 
-            if($scope.board[0] != '' && $scope.board[0] == $scope.board[1] && $scope.board[1] == $scope.board[2])
+            if($scope.game.board[0] != '' && $scope.game.board[0] == $scope.game.board[1] && $scope.game.board[1] == $scope.game.board[2])
             {
               youWon();
             }
-              else if($scope.board[3] != '' && $scope.board[3] == $scope.board[4] && $scope.board[4] == $scope.board[5])
+              else if($scope.game.board[3] != '' && $scope.game.board[3] == $scope.game.board[4] && $scope.game.board[4] == $scope.game.board[5])
             {
               youWon();
             }
-              else if($scope.board[6] != '' && $scope.board[6] == $scope.board[7] && $scope.board[7] == $scope.board[8])
+              else if($scope.game.board[6] != '' && $scope.game.board[6] == $scope.game.board[7] && $scope.game.board[7] == $scope.game.board[8])
             {
               youWon();
             }
-              else if($scope.board[0] != '' && $scope.board[0] == $scope.board[3] && $scope.board[3] == $scope.board[6])
+              else if($scope.game.board[0] != '' && $scope.game.board[0] == $scope.game.board[3] && $scope.game.board[3] == $scope.game.board[6])
             {
               youWon();
             }
-              else if($scope.board[1] != '' && $scope.board[1] == $scope.board[4] && $scope.board[4] == $scope.board[7])
+              else if($scope.game.board[1] != '' && $scope.game.board[1] == $scope.game.board[4] && $scope.game.board[4] == $scope.game.board[7])
             {
               youWon();
             }
-              else if($scope.board[2] != '' && $scope.board[2] == $scope.board[5] && $scope.board[5] == $scope.board[8])
+              else if($scope.game.board[2] != '' && $scope.game.board[2] == $scope.game.board[5] && $scope.game.board[5] == $scope.game.board[8])
             {
               youWon();
             }
-              else if($scope.board[0] != '' && $scope.board[0] == $scope.board[4] && $scope.board[4] == $scope.board[8])
+              else if($scope.game.board[0] != '' && $scope.game.board[0] == $scope.game.board[4] && $scope.game.board[4] == $scope.game.board[8])
             {
               youWon();
             }
-              else if($scope.board[2] != '' && $scope.board[2] == $scope.board[4] && $scope.board[4] == $scope.board[6])
+              else if($scope.game.board[2] != '' && $scope.game.board[2] == $scope.game.board[4] && $scope.game.board[4] == $scope.game.board[6])
             {
               youWon();
             }
@@ -125,6 +155,4 @@ $scope.clearBoard = function(){
               } 
           }
       };
-
-
 }); // End of controller
